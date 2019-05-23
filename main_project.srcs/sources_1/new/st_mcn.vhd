e51d2b699150e877665821ddb1edb37a1a533b3e
Tim Hadler <tch118@uclive.ac.nz>
2019-05-24 11:34:16 +1200

    Update st_mcn.vhd

--- main_project.srcs/sources_1/new/st_mcn.vhd
+++ main_project.srcs/sources_1/new/st_mcn.vhd
@@ -1,45 +1,32 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Author: Tim Hadler
 -- 
--- Create Date: 17.03.2019 15:18:43
--- Design Name: 
+-- 
+-- Create Date: 10.04.2019 11:09:58
 -- Module Name: st_mcn - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: ALU Design Project
+-- Target Devices: Nexys 4 DDR
+-- Tool Versions: Xilinx Vivado
+-- Description: This is the state machine module for the ALU.
+-- It uses a debounced button signal and 20Mhz clock signal
+-- to iterate through states. 
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity st_mcn is
     Port ( clk : in STD_LOGIC;
-           en : in STD_LOGIC;
+           en : in STD_LOGIC;   -- Debounced button signal
            state : out STD_LOGIC_VECTOR (2 downto 0));
 end st_mcn;
 
 architecture Behavioral of st_mcn is
 
 signal state_tmp : std_logic_vector (2 downto 0) := "001";
+
 begin
 process (clk, en)
     begin
