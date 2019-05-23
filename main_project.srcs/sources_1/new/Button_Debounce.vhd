76cb61bb1f092d3f5d53a60935b251bda81fb7a7
Tim Hadler <tch118@uclive.ac.nz>
2019-05-23 14:05:56 +1200

    Update Button_Debounce.vhd

--- main_project.srcs/sources_1/new/Button_Debounce.vhd
+++ main_project.srcs/sources_1/new/Button_Debounce.vhd
@@ -1,20 +1,14 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Author: Tim Hadler
 -- 
--- Create Date: 11.04.2019 15:15:26
--- Design Name: 
--- Module Name: Button_Debounce - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
 -- 
--- Dependencies: 
+-- Create Date: 10.04.2019 11:09:58
+-- Module Name: ALU - Behavioral
+-- Project Name: ALU Design Project
+-- Target Devices: Nexys 4 DDR
+-- Tool Versions: Xilinx Vivado
+-- Description: Is the button debounce module for the project
 -- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
 
@@ -22,14 +16,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+--Input clock runs at 5Hz
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Button_Debounce is
     Port ( Clk, Button : in STD_LOGIC;
