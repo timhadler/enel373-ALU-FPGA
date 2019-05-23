c05cee838aace25ed2c5afae9ce1ea2f3f33f0b7
Tim Hadler <tch118@uclive.ac.nz>
2019-05-23 14:04:33 +1200

    Update ALU.vhd

--- main_project.srcs/sources_1/new/ALU.vhd
+++ main_project.srcs/sources_1/new/ALU.vhd
@@ -1,20 +1,15 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Author: Tim Hadler
+-- 
 -- 
 -- Create Date: 10.04.2019 11:09:58
--- Design Name: 
 -- Module Name: ALU - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- Project Name: ALU Design Project
+-- Target Devices: Nexys 4 DDR
+-- Tool Versions: Xilinx Vivado
+-- Description: Is the ALU module used in the main project
+-- for the assignment
 -- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
 
@@ -23,14 +18,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ALU is
     Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
